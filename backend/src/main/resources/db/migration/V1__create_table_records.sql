6bdacc8987333af91f1f5b33ae082d69ab148709
Yuri Nascimento <yuriidiiego@gmail.com>
2023-05-23 12:20:29 -0300

    refactor: ♻️ Update records table schema
    In this commit, I made changes to the schema of the "records" table. The "speed" column is no longer set as "NOT NULL", allowing it to have a null value. Similarly, the "license_plate" and "vehicle_type" columns are also no longer set as "NOT NULL". These changes provide flexibility in inserting records with optional values in these columns.

--- backend/src/main/resources/db/migration/V1__create_table_records.sql
+++ backend/src/main/resources/db/migration/V1__create_table_records.sql
@@ -1,8 +1,8 @@
 CREATE TABLE records(
     id SERIAL PRIMARY KEY,
     created_at TIMESTAMP,
-    speed INTEGER NOT NULL,
-    license_plate VARCHAR(7) NOT NULL,
-    vehicle_type VARCHAR(20) NOT NULL
+    speed INTEGER,
+    license_plate VARCHAR(7),
+    vehicle_type VARCHAR(20)
 );
 
